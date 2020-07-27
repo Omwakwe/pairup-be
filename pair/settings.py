0d292a5cf9798f8f1436f4fe7d065e1a3968e66e
stephen remmi <stephenremmi@gmail.com>
2020-07-27 20:54:55 +0300

    Initialize branch for api set-up

--- pair/settings.py
+++ pair/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'cohort',
     'cloudinary',
     'django_extensions',
@@ -91,8 +92,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pair',
-        'USER': 'briankiiru',
-    'PASSWORD':'andela',
+        'USER': 'remmi',
+    'PASSWORD':'stephen',
     }
 }
 
