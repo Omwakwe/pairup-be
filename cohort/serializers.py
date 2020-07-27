edf66714205317c94e63965fc6f7d7a7c230e387
stephen remmi <stephenremmi@gmail.com>
2020-07-27 21:07:49 +0300

    import module for ModelSerializer

--- cohort/serializers.py
+++ cohort/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Cohort
+
+class CohortSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Cohort
+        fields = ['id','cohort_name','date_added']
