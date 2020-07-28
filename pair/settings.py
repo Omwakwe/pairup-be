e8a001faa240c434ef3ea50493246870ed290028
brayokenya <kiirubrian21.com>
2020-07-28 23:06:28 +0300

    configured CORS

--- pair/settings.py
+++ pair/settings.py
@@ -175,6 +175,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 
 
 # Extra places for collectstatic to find static files.
@@ -182,5 +184,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_URL = '/static/'
 
