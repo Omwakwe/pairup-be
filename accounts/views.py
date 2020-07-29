6c033d6d99c3f4756e18a2a7a7ae0a9fe40507f9
ian1017 <mdawidamengo@gmail.com>
2020-07-29 13:20:31 +0300

    add Account view

--- accounts/views.py
+++ accounts/views.py
@@ -3,39 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from rest_framework import viewsets
+from .serializers import AccountSerializer
+from .models import 
 
 
 
-
-# Create your views here.
-#def index(request):
-    #return render(request, 'index.html',)
-
-
-#def user_login(request):
-    
-    #if request.method == "POST":
-        #username = request.POST.get("username")
-        #password = request.POST.get("password")
-
-        #user = authenticate(username=username, password=password)
-
-        #if user:
-
-            #if user.is_active:
-                #login(request, user)
-
-                #return HttpResponseRedirect(reverse("index"))
-            #else:
-                #return HttpResponseRedirect(reverse("user_login")) #raise error/ flash
-
-        #else:
-            #return HttpResponseRedirect(reverse("user_login")) #raise error/ flash
-    #else:
-        #return render(request, "auth/login.html", context={})
-
-
-##@login_required
-#def user_logout(request):
-    #logout(request)
-    #return HttpResponseRedirect(reverse("user_login"))
+class AccountView(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
--- pair/settings.py
+++ pair/settings.py
@@ -128,14 +128,14 @@ WSGI_APPLICATION = 'pair.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pair',
-        'USER': 'ian',
-    'PASSWORD':'smart',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'pair',
+#         'USER': 'ian',
+#     'PASSWORD':'smart',
+#     }
+# }
 
 
 # Password validation
@@ -171,7 +171,15 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
