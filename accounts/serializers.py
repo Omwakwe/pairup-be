c57cdc74609c60061400b370ea735b524f0aad82
ian1017 <mdawidamengo@gmail.com>
2020-07-29 16:12:52 +0300

    add mentor serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -22,4 +22,24 @@ class StudentSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
-        
+
+class MentorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+                # fields = '__all__'
+        fields = ['id','email','first_name','last_name','user_name','cohort','bio','phone','last_login',]
+
+        # email = serializers.EmailField()
+        # username = serializers.CharField(max_length=20)
+
+
+    def create(self, validated_data):
+        validated_data['password'] = 'password'
+        return Account.objects.create_mentor(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.username = validated_data.get('content', instance.username)
+        instance.save()
+        return instance
+
