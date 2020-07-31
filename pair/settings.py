46172d38f80917e64a4556f697ab0ec4ab02424c
brayokenya <kiirubrian21.com>
2020-07-31 13:35:22 +0300

    api token

--- pair/settings.py
+++ pair/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pair',
-        'USER': 'remmi',
-    'PASSWORD':'stephen',
+        'USER': 'briankiiru',
+    'PASSWORD':'andela',
     }
 }
 
@@ -137,6 +137,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticatedOrReadOnly',)
+    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
     'DEFAULT_AUTHENTIACATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
